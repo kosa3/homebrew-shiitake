7f045e69f8bc9b1d9558b31229fb5969338b3cdc
kosa3 <19683276+kosa3@users.noreply.github.com>
2020-06-08 22:46:42 +0900

    v0.1.4 release

--- shiitake.rb
+++ shiitake.rb
@@ -1,8 +1,8 @@
 class Shiitake < Formula
   desc "しいたけ占い"
   homepage "https://github.com/kosa3/shiitake"
-  url "https://github.com/kosa3/shiitake/archive/v0.1.2.tar.gz"
-  sha256 "fb2fbfcfd42bc0c24ed3cbf9de6f5abc3d1312c68d6db728078d95c05c430093"
+  url "https://github.com/kosa3/shiitake/archive/v0.1.4.tar.gz"
+  sha256 "45b88d2f6ebfa5140e82276c2ab1773e0dc608a74d9f42c65db155c51d639cc8"
 
   depends_on "go" => :build
 
@@ -12,7 +12,6 @@ class Shiitake < Formula
     path.install buildpath.children
 
     cd path do
-        system 'go', 'get'
         system 'go', 'build'
         bin.install 'shiitake'
     end
